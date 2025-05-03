9fb57713b28c918fbdf88b2737bbc44e95c2a86a
Aditya singh <6306043783s@gmail.com>
2025-05-03 21:57:30 +0530

    fix error

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Navbar from './Components/Navbar'
+import Navbar from './pages/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
 import Skills from './pages/Skills'
